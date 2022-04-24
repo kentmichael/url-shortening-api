34216c08829c4c84eda4348cd40501dedab2bfba
Kent Michael San <kentmichaelsan22@gmail.com>
2022-04-25 00:59:10 +0800

    create shorten url feature

--- app/js/script.js
+++ app/js/script.js
@@ -1,59 +1,153 @@
 'use-strict'
 
+/*
+  Responsive landing page integrated with 
+  shrtcode API - https://app.shrtco.de/
+*/
+
 const menuButton = document.querySelector('#menu')
-const menuModal = document.querySelector('#menu-modal')
-
-const checkModalStatus = () => {
-  let status = menuModal.getAttribute('open')
-
-  return status===null ? false : true
-}
 
 menuButton.addEventListener('click', () => {
-  if(checkModalStatus()) return menuModal.close()
-  menuModal.show()
+  if(!document.querySelector('#menu-modal')) 
+    return createDialog()
+
+  document.querySelector('#menu-modal').close()
+  removeDialog()
 })
 
-/*
+const removeDialog = () => {
+  const nav = document.querySelector('.nav')
+  const dialog = document.querySelector('#menu-modal')
+  nav.removeChild(dialog)
+}
 
-*/
+const createDialog = () => {
+  const nav = document.querySelector('.nav')
+  const dialog = document.createElement('dialog')
+  const ul = document.createElement('ul')
+  const hr = document.createElement('hr')
+  const dialogContent = ['Features', 'Pricing', 'Resources', 'hr', 'Login', 'Sign Up']
+
+  dialogContent.forEach(element => {
+    const li = document.createElement('li')
+    const link = document.createElement('a')
+
+    if(element==='Sign Up') link.classList.add('cta')
+
+    if(element!=='hr') {
+      link.setAttribute('href', '#')
+      link.innerText = element
+      li.appendChild(link)
+    }else {
+      li.appendChild(hr)
+    }
+
+    ul.appendChild(li)
+  })
+
+  ul.classList.add('menu-links')
+  dialog.open = true
+  dialog.id = 'menu-modal'
+  dialog.classList.add('container')
+  dialog.appendChild(ul)
+  nav.appendChild(dialog)
+}
 
 class ShortenUrl {
   _shortenUrl = []
-
-  // constructor(input) {
-  //   this.shortenUrl = input
-  // }
+  _linkId = 0
 
   get shortenUrl() {
     return this._shortenUrl
   }
 
   set shortenUrl(input) {
-    fetch(`https://api.shrtco.de/v2/shorten?url=${input}`)
-    .then(response => {
-      if(response.status!==201) throw new Error(response.statusText)
-      return response.json()
-    })
-    .then(data => {
-      this._shortenUrl.push({
-        link: input, 
-        shortenedLink: data.result.full_short_link
+    if(Array.isArray(input)) {
+      this._shortenUrl = structuredClone(input)
+      this._linkId = this._shortenUrl[this._shortenUrl.length - 1].linkId
+      displayResults(this.shortenUrl)
+    }else {
+      fetch(`https://api.shrtco.de/v2/shorten?url=${input}`)
+      .then(response => {
+        if(response.status!==201) throw new Error(response.statusText)
+        return response.json()
       })
-      console.log(this._shortenUrl)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then(data => {
+        let id = this._linkId + 1
+        this._linkId = id
+        this._shortenUrl.push({
+          linkId: id,
+          link: input, 
+          shortenedLink: data.result.full_short_link
+        })
+        displayItem( id, input, data.result.full_short_link )
+        saveResultsToLocalStorage(this.shortenUrl)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   }
 }
 
-class DisplayShortenedLink extends ShortenUrl {
-
+function saveResultsToLocalStorage(results) {
+  localStorage.setItem('shortlyLinks', JSON.stringify(results))
+  console.log('Save item to localStorage...')
 }
 
-class CopyShortenedLink extends ShortenUrl {
-  
+function displayItem( linkId, link, shortenedLink ) {
+  const itemDiv = document.createElement('div')
+  const longLink = document.createElement('span')
+  const shortLink = document.createElement('span')
+  const copyLink = document.createElement('button')
+
+  longLink.classList.add('features__results--link')
+  longLink.innerText = link
+  shortLink.classList.add('features__results--shortenlink')
+  shortLink.innerText = shortenedLink
+  copyLink.innerText = 'Copy'
+  copyLink.setAttribute('id', 'copy-link')
+  copyLink.classList.add('cta', `link${linkId}`)
+  copyLink.addEventListener('click', () => {
+    copyShortenedLink( `link${linkId}`, shortenedLink )
+  })
+
+  itemDiv.append(longLink, shortLink, copyLink)
+  displayItemContainer(itemDiv)
+}
+
+function displayItemContainer(itemDiv) {
+  let resultContainer
+
+  if(document.querySelector('#features-result')) {
+    resultContainer = document.querySelector('#features-result')
+  }else {
+    const parent = document.querySelector('.features')
+    resultContainer = document.createElement('div')
+    resultContainer.setAttribute('id', 'features-result')
+    resultContainer.classList.add('features__results')
+    resultContainer.classList.add('container')
+    parent.insertAdjacentElement( "afterbegin", resultContainer )
+  }
+
+  return resultContainer.prepend(itemDiv)
+}
+
+function copyShortenedLink( linkId, link ) {
+  navigator.clipboard.writeText(link)
+  .then(() => {
+    const copyButton = document.querySelector(`.${linkId}`)
+    copyButton.innerText = 'Copied!'
+    copyButton.classList.add('features__results--copiedlink')
+  })
+  .catch(err => console.log(err))
+}
+
+function displayResults(items) {
+
+  items.forEach( ({linkId, link, shortenedLink}) => {
+    displayItem( linkId, link, shortenedLink )
+  })
 }
 
 const checkShortenUrlInput = input => {
@@ -68,7 +162,11 @@ const checkShortenUrlInput = input => {
 }
 
 const displayErrorMessage = errorMsg => {
-  const errorContainer = document.createElement('span')
+  let errorContainer = document.createElement('span')
+
+  if(document.querySelector('#error-msg'))
+    errorContainer = document.querySelector('#error-msg')
+
   const inputLabel = document.querySelector('.input-label')
   const urlInput = document.querySelector('#url-input')
   const shortenForm = document.querySelector('.shorten')
@@ -97,11 +195,17 @@ const shortenUrlForm = document.querySelector('.shorten')
 const shortenUrlInput = document.querySelector('#url-input')
 const user = new ShortenUrl()
 
+if(localStorage.getItem('shortlyLinks')) {
+  user.shortenUrl = JSON.parse(localStorage.getItem('shortlyLinks'))
+  console.log('Load items from localStorage...')
+}
+
 shortenUrlForm.addEventListener('submit', ev => {
   ev.preventDefault()
 
   if(checkShortenUrlInput(shortenUrlInput.value)){
     user.shortenUrl = shortenUrlInput.value
+    shortenUrlInput.value = ''
     clearErrorMessage()
   }
 })
